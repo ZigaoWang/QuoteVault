80c14f774584fa40dd6ef68c44a402156f568f4c
Zigao Wang <zigao.wang24@gmail.com>
2025-07-09 18:38:26 +0800

    feat: empty xcode project

---
+++ QuoteVault/QuoteVault/QuoteVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteVaultApp.swift
+//  QuoteVault
+//
+//  Created by Zigao Wang on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct QuoteVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
